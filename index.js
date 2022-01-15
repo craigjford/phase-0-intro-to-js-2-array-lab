256d3b54f17d8cd8d54dcf3096b3f1ed09db28f0
Craig Ford <craig_ford@icloud.com>
2022-01-15 16:13:31 -0500

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield")
+
+});
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+    console.log(cats);
+} 
+
+//beforeEach();
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+    console.log(cats);
+}
+
+//beforeEach();
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    console.log(cats);
+}
+
+//beforeEach();
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    console.log(cats);
+}
+
+//beforeEach();
+
+function appendCat(name) {
+    const copyCat = [...cats, name];
+    console.log(copyCat);
+    return copyCat;
+}
+
+function prependCat(name) {
+    const prepCat = [name, ...cats];
+    console.log(prepCat);
+    return prepCat; 
+}
+
+function removeLastCat() {
+    const byeCat = cats.slice(0, 2);
+    console.log(byeCat);
+    return byeCat; 
+}
+
+function removeFirstCat() {
+    const byeFirstCat = cats.slice(1);
+    console.log(byeFirstCat);
+    return byeFirstCat; 
+}
